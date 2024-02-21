30096f8763321faab06a0d5f1df943f15a9e7227
yonatanShnits <shnits@post.bgu.ac.il>
2024-02-21 16:11:37 +0200

    added stop recording and saving audio file

--- frontend/VoiceCoach/Assets/Scenes/project/scripts/project_script.cs
+++ frontend/VoiceCoach/Assets/Scenes/project/scripts/project_script.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using NAudio.Wave;
 
 public class project_script : MonoBehaviour
 {
 
     public GameObject vis;
     private List<GameObject> visRecordingArray = new List<GameObject>();
-    private List<GameObject> visSampleArray = new List<GameObject>();
+    //Not sure if needed:
+    //private List<GameObject> visSampleArray = new List<GameObject>();
 
     public float minHeight;
     public float maxHeight;
@@ -18,6 +20,8 @@ public class project_script : MonoBehaviour
     public int visualizerSimples;
 
     public AudioSource audioSource;
+    public AudioClip recordedAudioClip;
+    private bool isRecording = false;
 
 
     void Start()
@@ -29,7 +33,7 @@ public class project_script : MonoBehaviour
         visualizerSimples = 1024;
 
         startVis();
-        addMicForRecording();
+        //addMicForRecording();
 
     }
 
@@ -45,14 +49,36 @@ public class project_script : MonoBehaviour
         }
     }
 
-    private void addMicForRecording()
-    {
-        audioSource.clip = Microphone.Start(string.Empty, audioSource.loop, 10, 44100);
-        audioSource.loop = true;
-        while (!(Microphone.GetPosition(null) > 0)) { }
 
-        audioSource.Play();
+    public void startRecording()
+    {
+        if (!isRecording)
+        {
+            Debug.Log("Start Recording was invoked");
+            recordedAudioClip = audioSource.clip = Microphone.Start(string.Empty, audioSource.loop, 15, 44100);
+            audioSource.loop = true;
+            //while (!(Microphone.GetPosition(null) > 0)) { }
+            audioSource.Play();
+            isRecording = true;
+        }
     }
+
+
+    public void stopRecording()
+    {
+        Debug.Log("Stop Recording was invoked");
+        if (isRecording)
+        {
+            Microphone.End(null);
+            isRecording = false;
+            audioSource.clip = null; // Stop playing sound from the microphone
+            string filePath = Application.persistentDataPath + "/recordedAudio.wav";
+            AudioRecorder.SaveRecording(recordedAudioClip, filePath);
+            Debug.Log("Recording saved to: " + filePath);
+            isRecording = false;
+        }
+    }
+
 
     void FixedUpdate()
     {
